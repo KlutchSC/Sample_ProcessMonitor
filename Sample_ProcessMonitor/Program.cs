db2d7e1ed97a448fcc408457158a8cf4c16d0bac
KlutchSC <jdandrews108@gmail.com>
2016-06-21 10:57:24 -0700

    Initial Commit
    initial commit of the the process monitor

---
+++ Sample_ProcessMonitor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Sample_ProcessMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                if (Console.Read() == 's') { ShowProcesses();}
+
+
+                if (Console.Read() == 'q')
+                    break;
+            }
+        }
+
+        private static void ShowProcesses()
+        {
+            Process[] localAll = Process.GetProcesses();
+            foreach (var proc in localAll)
+            {
+                Console.WriteLine(proc.ProcessName);
+            }
+        }
+    }
+}
