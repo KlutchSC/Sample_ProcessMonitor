ef5598727c75d1ec5bafc14a0c33a799bfeb9394
KlutchSC <jdandrews108@gmail.com>
2016-06-21 11:07:49 -0700

    search methods
    added new methods to search for a process that is running on the local
    machine by Name and Id

--- Sample_ProcessMonitor/Program.cs
+++ Sample_ProcessMonitor/Program.cs
@@ -5,25 +5,51 @@ namespace Sample_ProcessMonitor
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             while (true)
             {
-                if (Console.Read() == 's') { ShowProcesses();}
+                if (Console.ReadLine() == "show")
+                {
+                    ShowProcesses();
+                }
 
+                if (Console.ReadLine() == "name")
+                {
+                    Console.WriteLine("Enter the process name:");
+                    ShowProcessByName(Console.ReadLine());
+                }
 
-                if (Console.Read() == 'q')
+                if (Console.ReadLine() == "id")
+                {
+                    Console.WriteLine("Enter the process id:");
+                    ShowProcessById(Convert.ToInt32(Console.ReadLine()));
+                }
+
+                if (Console.ReadLine() == "quit")
                     break;
             }
         }
 
         private static void ShowProcesses()
         {
-            Process[] localAll = Process.GetProcesses();
-            foreach (var proc in localAll)
+            var process = Process.GetProcesses();
+            foreach (var proc in process)
             {
                 Console.WriteLine(proc.ProcessName);
             }
+        }
+
+        private static void ShowProcessByName(string procName)
+        {
+            var process = Process.GetProcessesByName(procName);
+            Console.WriteLine(process);
+        }
+
+        private static void ShowProcessById(int procId)
+        {
+            var process = Process.GetProcessById(procId);
+            Console.WriteLine(process);
         }
     }
 }
